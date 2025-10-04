4c28cd4ca667c468d60d03033ece010188525b43
developerasun <nellow1102@gmail.com>
2025-10-04 20:18:35 +0900

    test: check favicon and search index at absolute path

--- docs/vocs.config.ts
+++ docs/vocs.config.ts
@@ -3,7 +3,17 @@ import { defineConfig } from "vocs";
 export default defineConfig({
   title: "DeveloperAsun",
   // @dev should be set to repo name for github pages
-  basePath: "developerasun",
+  basePath: "/developerasun",
+  iconUrl: { light: "/favicon.ico", dark: "/favicon.ico" },
+  logoUrl: { light: "/logo.jpg", dark: "/logo.jpg" },
+  // search: {
+  //   boostDocument(documentId) {
+  //     if (documentId.startsWith("pages/docs")) return 3;
+  //     if (documentId.startsWith("pages/account-abstraction")) return 2;
+  //     if (documentId.startsWith("pages/experimental")) return 2;
+  //     return 1;
+  //   },
+  // },
   sidebar: [
     {
       text: "About",
