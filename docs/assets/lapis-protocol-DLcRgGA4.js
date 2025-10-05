638b3b523c8f628fb193bebf80cc0f7d6d399ac0
developerasun <nellow1102@gmail.com>
2025-10-06 03:39:00 +0900

    build: ship to prod

---
+++ docs/assets/lapis-protocol-DLcRgGA4.js
@@ -0,0 +1,19 @@
+import{u as s,j as e}from"./index-Bi9O2lK2.js";const o={title:"Projects | Lapis Protocol",description:"undefined"};function n(i){const t={a:"a",div:"div",h1:"h1",header:"header",li:"li",p:"p",ul:"ul",...s(),...i.components};return e.jsxs(e.Fragment,{children:[e.jsx(t.header,{children:e.jsxs(t.h1,{id:"projects--lapis-protocol",children:["Projects | Lapis Protocol",e.jsx(t.a,{"aria-hidden":"true",tabIndex:"-1",href:"#projects--lapis-protocol",children:e.jsx(t.div,{"data-autolink-icon":!0})})]})}),`
+`,e.jsx(t.p,{children:"Play to earn Android app, made with teammates:"}),`
+`,e.jsxs(t.ul,{children:[`
+`,e.jsx(t.li,{children:"tech: 2 mobile, 2 backend, 1 blockchain(me)"}),`
+`,e.jsx(t.li,{children:"product: 1 pm, 1 po, 1 designer"}),`
+`]}),`
+`,e.jsx(t.p,{children:"Mainly responsible for handling smart contract and web3 server."}),`
+`,e.jsxs(t.p,{children:[e.jsx(t.a,{href:"https://play.google.com/store/apps/details?id=com.mithril.lapis",children:"Project itself"}),` might have been dead(or alive) after I left the company,
+but smart contracts I did still exist on Optimism network(and they forever will).`]}),`
+`,e.jsx(t.p,{children:"Check out a list of verified smart contracts I coded during the project."}),`
+`,e.jsxs(t.ul,{children:[`
+`,e.jsx(t.li,{children:e.jsx(t.a,{href:"https://optimism.blockscout.com/token/0x52F7A03f16fDd984458D88a7e9cd74256b328a3b",children:"ERC20"})}),`
+`,e.jsx(t.li,{children:e.jsx(t.a,{href:"https://optimism.blockscout.com/address/0xEE8e2B125F6eeD2da17dCE622B4caccE78d5578c",children:"ERC721"})}),`
+`,e.jsx(t.li,{children:e.jsx(t.a,{href:"https://optimism.blockscout.com/token/0x0Eea4043EB43143D2b2121c3cA7083161eAb67Df",children:"ERC1155"})}),`
+`,e.jsx(t.li,{children:e.jsx(t.a,{href:"https://optimism.blockscout.com/address/0x473B185e92ed5f4E5e2C8A00C7880ceD029A6EA9",children:"Reward utility"})}),`
+`,e.jsx(t.li,{children:e.jsx(t.a,{href:"https://explorer.optimism.io/address/0xD7EaBf942a446D278AfD9a00B4aAe188b0664B21",children:"Upgradeable: proxy"})}),`
+`,e.jsx(t.li,{children:e.jsx(t.a,{href:"https://explorer.optimism.io/address/0x19A849cfA6588AA206BB0A8f65b46B08d1D1c3F4",children:"Upgradeable: implementation"})}),`
+`]}),`
+`,e.jsx(t.p,{children:"Note that these have been all public at the moment of deployment while upgradeable implementaiton's ABI not disclosed for the security."})]})}function c(i={}){const{wrapper:t}={...s(),...i.components};return t?e.jsx(t,{...i,children:e.jsx(n,{...i})}):n(i)}export{c as default,o as frontmatter};
