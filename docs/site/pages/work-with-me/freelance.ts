388f8852e5050956958e5fc5e3fe67ce35f6a76e
developerasun <nellow1102@gmail.com>
2025-10-09 23:32:33 +0900

    doc: embed paypal qrcode

--- docs/site/pages/work-with-me/freelance.ts
+++ docs/site/pages/work-with-me/freelance.ts
@@ -1,3 +1,6 @@
-export { GOOGLE_FORM_URL };
+export { GOOGLE_FORM_URL, VOUCHER_CATALOGS };
 
 const GOOGLE_FORM_URL = "https://forms.gle/SR4nZcuefWRJE15RA" as const;
+const VOUCHER_CATALOGS = {
+  "5": "voucher/VOUCHER1.png",
+} as const;
