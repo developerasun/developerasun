2be09717349420b6913ab822d83c6133e2289096
developerasun <nellow1102@gmail.com>
2025-11-07 17:38:45 +0900

    feat: setup kor/eng translation by key name

--- docs/site/components/international.tsx
+++ docs/site/components/international.tsx
@@ -3,28 +3,34 @@ import { i18n } from "../pages/index";
 
 export function SetUpTranslation() {
   return (
-    <>
+    <div
+      id="setup"
+      className="rounded-md p-2 border-2 border-white-500 border-solid"
+      style={{
+        display: "grid",
+        placeItems: "center",
+        gridAutoFlow: "column",
+        gap: "1rem",
+      }}
+    >
       <button
         type="button"
         onClick={async () => await i18n.changeLanguage("en")}
       >
         Eng
       </button>
+      <span>|</span>
       <button
         type="button"
         onClick={async () => await i18n.changeLanguage("kr")}
       >
         한국어
       </button>
-    </>
+    </div>
   );
 }
 
 export function RenderTranslation({ tKey }: { tKey: string }) {
   const { t } = useTranslation();
-  return (
-    <>
-      <span>{t(tKey)}</span>
-    </>
-  );
+  return <>{t(tKey)}</>;
 }
